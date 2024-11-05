cc16827dbb3388d05992373763c3630522e923bf
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2024-11-05 23:37:00 +0500

    language switcher added

--- src/js/main.js
+++ src/js/main.js
@@ -6,27 +6,54 @@ AOS.init();
 
 const langSelect = document.querySelector('#languageSwitcher')
 const allLanguages = ['en', 'ru', 'uz']
+const languageElements = document.querySelectorAll('.languages a')
 
-langSelect.addEventListener('change', changeURLlanguage)
-
-function changeURLlanguage() {
-  let lang = langSelect.value;
-  location.href = window.location.pathname + `#${lang}`
+const hash = window.location.hash.substring(1)
+if (hash == '' || !allLanguages.includes(hash)) {
+  location.href = window.location.pathname + '#en'
   location.reload()
 }
+languageElements.forEach(el => {
+  if (el.attributes.language.value == hash)
+    el.classList.add('active')
+})
 
-function changelanguage() {
-  const hash = window.location.hash.substring(1)
-  if (!allLanguages.includes(hash)) {
-    location.href = window.location.pathname + '#ru'
-    location.reload()
-  }
-  langSelect.value = hash
-  for (let key in languages) {
-    document.querySelector('.lng-' + key).innerHTML = languages[key][hash]
-  }
+for (let key in languages) {
+  document.querySelector('.lng-' + key).textContent = languages[key][hash]
 }
-changelanguage()
+
+
+languageElements.forEach(el => el.addEventListener('click', () => {
+  document.querySelector('.active').classList.remove('active');
+  el.classList.add('active')
+  const currentLang = el.getAttribute('language');
+  location.href = window.location.pathname + `#${currentLang}`
+  for (let key in languages) {
+    document.querySelector('.lng-' + key).textContent = languages[key][currentLang]
+  }
+
+}))
+
+
+// languageElements.addEventListener('click', changelanguage())
+
+// function changeURLlanguage() {
+//   let lang = langSelect.value;
+//   location.href = window.location.pathname + `#${lang}`
+//   location.reload()
+// }
+
+// function changelanguage() {
+//   const hash = window.location.hash.substring(1)
+//   if (!allLanguages.includes(hash)) {
+//     location.href = window.location.pathname + '#ru'
+//     location.reload()
+//   }
+//   langSelect.value = hash
+//   for (let key in languages) {
+//     document.querySelector('.lng-' + key).innerHTML = languages[key][hash]
+//   }
+// }
 
 $(document).ready(function () {
   $('.single-item').slick({
